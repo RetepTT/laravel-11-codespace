3808ec130bec5c2e1e822299ce7a6fe76ad69448
RetepTT <peter.howett@live.com>
2024-05-15 11:00:28 +0000

    Updated code to follow guide for codespace setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,49 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+            
+        }),
+        
+
     ],
+    server: {
+        hmr: {
+            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.app.github.dev' : null,
+            clientPort: process.env.CODESPACE_NAME ? 443 : null,
+            protocol: process.env.CODESPACE_NAME ? 'wss' : null,
+            
+        },
+        cors: true
+    }
+    // server: {
+        // host : 'https://upgraded-doodle-vppw9j9vvxr2w4j4-3000.app.github.dev/',
+        // port: 3000,
+        
+        // hmr: {
+            // host: 'upgraded-doodle-vppw9j9vvxr2w4j4-5173.app.github.dev',
+            // clientPort: ''
+        // },
+        // cors: 'true',
+        // https: 'true'
+    //   }
+    // server: {
+    //     hmr: {
+    //         host: 'upgraded-doodle-vppw9j9vvxr2w4j4-5173.app.github.dev',
+    //     },
+    // },
 });
+
